9ad0ea9f117625e440fed7d15bd05eeb3c5a92bd
Divin <kdivin@icloud.com>
2023-09-29 00:12:56 +0200

    Initial Commit

---
+++ AllMyApps_SE/AllMyApps_SEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllMyApps_SEApp.swift
+//  AllMyApps_SE
+//
+//  Created by Divin Karagira on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct AllMyApps_SEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
